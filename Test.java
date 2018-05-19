83e7566e6eb0560782a22497e9c9691c8e659346
speedycatfish <ymcacolin@gmail.com>
2018-05-19 14:44:25 -0700

    Reads input

---
+++ Test.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		loop: while (true) {
+			String input = scan.nextLine();
+			switch (input) {
+			case "start":
+
+				break;
+			case "create":
+
+				break;
+			case "stop":
+				break loop;
+			}
+		}
+		scan.close();
+	}
+
+	void TakeTest() {
+
+	}
+
+	void CreatTest() {
+
+	}
+}
+
