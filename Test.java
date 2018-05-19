0cf1d0c8bdc8095b53324ec7e7e19bf5948e8a92
speedycatfish <ymcacolin@gmail.com>
2018-05-19 15:24:14 -0700

    Get options

--- Test.java
+++ Test.java
@@ -12,8 +12,8 @@ public class Test {
 				TakeTest();
 				break;
 			case "create":
-				String question = scan.nextLine();
-				CreateTest(question);
+
+				CreateTest();
 				break;
 			case "stop":
 				break loop;
@@ -26,7 +26,19 @@ public class Test {
 
 	}
 
-	static void CreateTest(String question) {
-
+	static void CreateTest() {
+		System.out.println("Write your question");
+		String question = scan.nextLine();
+		System.out.println("Please type in the correct answer");
+		String c = scan.nextLine();
+		System.out.println("Please enter the first wrong answer");
+		String w1 = scan.nextLine();
+		System.out.println("Please enter the second wrong answer");
+		String w2 = scan.nextLine();
+		System.out.println("Please enter the third wrong answer");
+		String w3 = scan.nextLine();
+		System.out.println("Please enter the fourth wrong answer");
+		String w4 = scan.nextLine();
+		String options = c + ",~," + w1 + ",~," + w2 + ",~," + w3 + ",~," + w4;
 	}
 }
